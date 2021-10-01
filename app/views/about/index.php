a33a1d28c22981936644bcc1a129e3cb655c0292
193040126 <193040126.diva@mail.unpas.ac.id>
2021-10-01 15:52:55 +0700

    menyelesaikan video ke 6 terakit dengan assets dan menambahkan bootstrap ke dalam folder css dan js

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,4 +1,7 @@
+<div class="container">
 
-    <h1>About Me</h1>
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/profil lingkaran.jpeg" alt="Diva Amwal" width="200" class="shadow">
     <p>Halo, nama saya <?= $data['nama'];?>, umur saya <?= $data['umur'];?> tahun, 
     saya adalah seorang <?= $data['pekerjaan'];?>.</p>
+</div>
